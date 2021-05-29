e8205d0f12ef6e64ecc53542be8288e333f40748
Auroiah Morgan <amwebdev@outlook.com>
2021-05-29 19:17:16 -0400

    Update commands

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Slugg Bot
 
+# Commands
+ $user-info | Display Info about yourself
+
 ## [Report Issues](https://github.com/amwebdev86/slugg-bot/issues)
--- commands/fun/moderation/util/user-info.js
+++ commands/fun/moderation/util/user-info.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'user-info',
 	description: 'Display Info about yourself',
+	args: false,
 	execute(message) {
 		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	},
