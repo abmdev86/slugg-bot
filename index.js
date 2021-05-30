3b582d43de925a24d79dcbfba084c79af0f03a1f
Auroiah Morgan <amwebdev@outlook.com>
2021-05-29 22:04:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ client.commands = new Discord.Collection();
 const prefix = '$';
 
 const commandFiles = [{
-	name: 'hello', description: ' Say Hello to the bot', execute(message) {
+	name: 'hello',
+	description: ' Test command to see if the bot is taking commands and args',
+	execute(message) {
 		message.reply(`Hello ${message.author}`);
 	},
+}, {
+	name: 'good',
+	description: ' Say Hello to the bot',
+	execute(message, args) {
+		if (!args.length) {
+			message.channel.send('Yes, Yes I am good, and I get better everyday....');
+		}
+		else if (args[0].toLowerCase() === 'bot') {
+			message.reply(`Thank you ${message.author}`);
+
+		}
+	},
 },
-{ name: 'server-info', description: 'Get information about the server' },
+{
+	name: 'server-info',
+	description: 'Get information about the server',
+	execute(message) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	},
+},
 ];
 
 for (const cmd of commandFiles) {
@@ -21,7 +41,14 @@ client.once('ready', () => console.log('Slugg-Bot Logged on server'));
 
 client.on('shardError', error => console.error(error));
 
+client.on('guildMemberAdd', member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'introductions');
+	if (!channel) return;
+	channel.send(`Yo ${member} welcome to SluggaVille, please take some time to introduce yourself, \n Job? \n Skill? \n Talent?, or hobbies.\n
+	can be anything even just here to make friends and game. Welcome aboard but dont be a lurker or ghost`);
+});
 client.on('message', (message) => {
+
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
